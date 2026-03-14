5e57231f616f5409e0639420919d4acf796c2bc2
Sagar Thakur <161070652+MrShanu1423@users.noreply.github.com>
2026-03-15 00:05:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ bubble.style.animationDuration = 5+Math.random()*10+"s"
 bg.appendChild(bubble)
 
 }
+const animation = document.querySelector(".bg-animation");
+
+for(let i=0;i<60;i++){
+
+const span = document.createElement("span");
+
+span.style.left = Math.random()*100 + "vw";
+span.style.animationDuration = (Math.random()*5+5) + "s";
+span.style.opacity = Math.random();
+
+animation.appendChild(span);
+
+}
